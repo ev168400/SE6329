5362cb52ec79264e6068a79101f73ce596d3d8ea
Eddie Villarreal <ejv190000@utdallas.edu>
2023-12-03 13:07:13 -0600

    Table class and Table Management System

---
+++ src/Menu_Item.java
@@ -0,0 +1,15 @@
+public class Menu_Item {
+    int itemID;
+    String itemName;
+    String itemCategory;
+    int quantityOrdered;
+    double price;
+
+    public Menu_Item(int itemID, String itemName, String itemCategory, int quantityOrdered, double price){
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemCategory = itemCategory;
+        this.quantityOrdered = quantityOrdered;
+        this.price = price;
+    }
+}
