3fa9a8c2c8f2b1de9c6a36f3e789eca8680f2eeb
mshariqazeem <92411045+mshariqazeem@users.noreply.github.com>
2023-12-06 15:47:32 -0600

    Update OrderManagementSystem.java

--- src/OrderManagementSystem.java
+++ src/OrderManagementSystem.java
@@ -1,3 +1,33 @@
-public class OrderManagementSystem {
+import java.io.FileWriter;
+
+public class OrderManagementSystem
+{
+	public static CustomerOrderRecord SelectPlaceOrder(TableRecord tabRecord)
+	{
+		CustomerOrderRecord order = new CustomerOrderRecord(tabRecord);
+		storeOrder(order);
+		OrderQueueManagementSystem.placeOrder(order);
+		return order;
+	}
+	
+	public static void storeOrder(CustomerOrderRecord order)
+	{
+		try
+		{
+			FileWriter fw = new FileWriter("src/DB/OrderDB", true);
+
+			fw.write(CustomerOrderRecord.orderID + ", ");
+			fw.write(order.tableID + ", ");
+			fw.write(order.numberOfItems + ", [");
+			for(int i = 0; i < order.Items.size(); i++)
+				fw.write(order.Items.get(i).name + ", ");
+			fw.write("], " + order.paymentTotal + "\n");
     
+            fw.close(); 
+        } 
+        catch (Exception e)
+		{ 
+            e.getStackTrace(); 
+        }
+	}
 }
