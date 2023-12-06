a2e7f4e53079fdf627b5df2c3d6419f3ae522995
AngeleenAbesamis <70412244+AngeleenAbesamis@users.noreply.github.com>
2023-12-06 13:08:35 -0600

    Update FloorPlanManagementSystem.java

--- src/FloorPlanManagementSystem.java
+++ src/FloorPlanManagementSystem.java
@@ -1,3 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FloorPlanManagementSystem {
-    
+    private Map<Integer, TableRecord> floorPlan;
+
+    public FloorPlanManagementSystem() {
+        this.floorPlan = new HashMap<>();
+        initializeFloorPlanFromDB("TableDB.csv");
+    }
+
+    private void initializeFloorPlanFromDB(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                int tableID = Integer.parseInt(parts[0]);
+                String tableStatus = parts[1].trim(); // Remove leading/trailing whitespaces
+
+                TableRecord table = new TableRecord();
+                table.initializeTab(tableID);
+                table.table_status = tableStatus;
+
+                floorPlan.put(tableID, table);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayFloorStatus() {
+        System.out.println("Floor Status:");
+        for (Map.Entry<Integer, TableRecord> entry : floorPlan.entrySet()) {
+            int tableID = entry.getKey();
+            TableRecord table = entry.getValue();
+            System.out.println("Table ID: " + tableID + ", Status: " + table.table_status);
+        }
+    }
+
+    public TableRecord getTable(int tableID) {
+        return floorPlan.get(tableID);
+    }
 }
