4b72f7ab542f274eaf2f43b3fe6f4e18af830a2e
mshariqazeem <92411045+mshariqazeem@users.noreply.github.com>
2023-12-06 15:46:47 -0600

    Update CustomerOrderRecord.java

--- src/CustomerOrderRecord.java
+++ src/CustomerOrderRecord.java
@@ -1,3 +1,20 @@
-public class CustomerOrderRecord {
-    
+import java.util.ArrayList;
+
+public class CustomerOrderRecord
+{
+	static int orderID = 0;
+	int tableID, numberOfItems;
+	double paymentTotal;
+	ArrayList<Menu_Item> Items;
+	
+	public CustomerOrderRecord(TableRecord tabRecord)
+	{
+		orderID++;
+		tableID = tabRecord.table_ID;
+		numberOfItems = tabRecord.table_tab.size();
+		Items = tabRecord.table_tab;
+		
+		for(int i = 0; i < Items.size(); i++)
+			paymentTotal += Items.get(i).price; 
+	}
 }
