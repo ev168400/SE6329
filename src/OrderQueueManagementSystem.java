fc9a0909bc6b6859e2ee67821e80e94f51acf3b9
mshariqazeem <92411045+mshariqazeem@users.noreply.github.com>
2023-12-06 15:47:11 -0600

    Update OrderQueueManagementSystem.java

--- src/OrderQueueManagementSystem.java
+++ src/OrderQueueManagementSystem.java
@@ -1,3 +1,11 @@
-public class OrderQueueManagementSystem {
-    
+import java.util.ArrayList;
+
+public class OrderQueueManagementSystem
+{
+	static ArrayList<CustomerOrderRecord> orderQueue = new ArrayList<>();
+	
+	public static void placeOrder(CustomerOrderRecord order)
+	{
+		orderQueue.add(order);
+	}
 }
